adea63ea6b37cff80eb19d763c448d72ce93438d
aishwarya <aishjaiswal.com>
2022-03-25 01:32:30 -0400

    youtube video class

---
+++ src/day2/YouTubeVideo.java
@@ -0,0 +1,27 @@
+package day2;
+
+public class YouTubeVideo {
+    //state
+
+    //videoId
+    //video
+    //thumbnailImage
+    //videoTitle
+    //videoDescription
+    //channelName
+    //numberOfLikes
+    //numberOfDislike
+    //numberOfComment
+    //numberOfSubscriber
+    //numberOfViews
+    //postedTime
+
+    //Behavior or Function
+
+    //playVideo
+    //saveVideo
+    //download
+    //pause
+    //share
+    //canComment
+}
