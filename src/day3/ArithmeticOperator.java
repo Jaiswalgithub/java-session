e7b2f70bff15deedf3e73894b1fb6a97b57935ac
aishwarya <aishjaiswal.com>
2022-03-29 10:20:39 -0400

    Arithmetic Operator

--- src/day3/ArithmeticOperator.java
+++ src/day3/ArithmeticOperator.java
@@ -3,7 +3,7 @@ package day3;
 public class ArithmeticOperator {
     public static void main(String[] args) {
 
-        int a= 2, b=3, c=5;
+        int a= 2, b= 3, c= 5;
         int temp;
         temp = ((a+b)*c);
         System.out.println("Result is:" +temp);
