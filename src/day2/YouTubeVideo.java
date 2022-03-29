a5ee77719189cb126a67554871054c4b2a67b0d1
aishwarya <aishjaiswal.com>
2022-03-29 01:03:18 -0400

    datatype modified

--- src/day2/YouTubeVideo.java
+++ src/day2/YouTubeVideo.java
@@ -3,17 +3,33 @@ package day2;
 public class YouTubeVideo {
     //state
     //videoId
+    String videoID;
     //video
+    String video;
     //thumbnailImage
+    String thumbnailImage;
     //videoTitle
+    String videoTitle;
     //videoDescription
+    String videoDescription;
     //channelName
+    String channelName;
+    //channelThumbnail
+    String channelThumbnail;
     //numberOfLike
+    long numberOfLike;
     //numberOfDislike
+    long numberOfDislike;
     //numberOfComment
+    long numberOfComment;
     //numberOfSubscriber
+    long numberOfSubscriber;
     //numberOfViews
+    long numberOfViews;
     //postedTime
+    String postedTime;
+    //isDownloadable
+    boolean isDownloadable;
 
     //Behavior or Function
     //playVideo
@@ -22,4 +38,12 @@ public class YouTubeVideo {
     //pause
     //share
     //canComment
+    //next
+    //canLike
+    //canDislike
+    //autoplay
+    //adjustVolume
+    //screenMode
+    //sortBy
+    //settings
 }
