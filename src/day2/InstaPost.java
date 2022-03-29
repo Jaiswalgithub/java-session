d49e0e2ea703416058ba9ef0f42b5b5ecc12da37
aishwarya <aishjaiswal.com>
2022-03-29 01:21:50 -0400

    Instagram post

---
+++ src/day2/InstaPost.java
@@ -0,0 +1,23 @@
+package day2;
+
+public class InstaPost {
+    //state
+    String postedBy;
+    String userImage;
+    boolean isVideo;
+    String caption;
+    long numberOfLike;
+    long numberOfComment;
+    String location;
+    String postedTime;
+
+    //Behaviour or Function
+    //editPost
+    //deletePost
+    //share
+    //save
+    //likePost
+    //commentPost
+    //tagPeople
+
+}
