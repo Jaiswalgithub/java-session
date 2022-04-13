65f76f9e69b805ace719b947b073ae8f46931235
aishwarya <aishjaiswal.com>
2022-04-13 19:22:37 -0400

    Relational Operator

---
+++ src/day3/RelationalOperator.java
@@ -0,0 +1,40 @@
+package day3;
+
+import java.sql.SQLOutput;
+
+public class RelationalOperator {
+
+    public static void main(String[] args){
+       // int m = 2;
+       // int n = 3;
+
+      // boolean isEqual = (m == n);
+      // System.out.println(isEqual);
+
+       // boolean isNotEqual = (m!=n);
+       // System.out.println(isNotEqual);
+
+       //boolean isLessThan = (m<n);
+        //System.out.println(isLessThan);
+
+        //boolean isGreaterThanOrEqualTo = (m >= n);
+        //System.out.println(isGreaterThanOrEqualTo);
+
+        //boolean m = true;
+        //boolean n = false;
+
+        //boolean anding = (m&&n);
+        //System.out.println(anding);
+
+        //boolean or = (m || n);
+        //System.out.println(or);
+
+        int age = 14;
+        String b1 = (age >= 18) ? "You are allowed" : "you are not allowed" ;
+        System.out.println(b1);
+
+
+
+
+    }
+}
