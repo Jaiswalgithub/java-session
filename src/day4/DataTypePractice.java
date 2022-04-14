6e4419a205ce0a3a1645957169a8f432786b38a3
aishwarya <aishjaiswal.com>
2022-04-13 23:31:05 -0400

    datatype practice

--- src/day4/DataTypePractice.java
+++ src/day4/DataTypePractice.java
@@ -1,8 +1,15 @@
 package day4;
-
+//primitive data type give default value 0, boolean give false and string give null value
 public class DataTypePractice {
 
-    static int a;
+    //static int a;
+    //static float a;
+    //static double a;
+    //static long a;
+    //static byte a;
+    //static char a;
+    //static String a;
+    static boolean a;
 
     public static void main(String[] args){
 
