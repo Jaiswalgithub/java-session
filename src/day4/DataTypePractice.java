b01b93513988d0d95ae17210af722e904c0891a0
aishwarya <aishjaiswal.com>
2022-04-13 23:18:56 -0400

    datatype practice

--- src/day4/DataTypePractice.java
+++ src/day4/DataTypePractice.java
@@ -4,9 +4,9 @@ public class DataTypePractice {
 
     static int a;
 
-    public void main(String[] args){
+    public static void main(String[] args){
 
         System.out.println(a);
-
     }
+
 }
